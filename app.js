9f5dfa062f700d3c0290b2e760afa905532fb11d
Charles Zhang <gzcharleszhang@hotmail.com>
2018-01-20 12:05:16 -0500

    Bug Fix

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/api', index);
+app.use('/', index);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
