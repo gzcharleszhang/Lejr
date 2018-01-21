8062df0f023cddccdf6a62d3af14aae40e5fc657
Charles Zhang <gzcharleszhang@hotmail.com>
2018-01-21 01:29:05 -0500

    debugging updates

--- routes/index.js
+++ routes/index.js
@@ -184,13 +184,14 @@ router.get('/ledger', function(req, res){
 
 // POST method for notifications (callback from Interac)
 router.post('/notifications', function(req, res, next){
+
+  var updates = req.body.moneyRequestUpdates[0];
     var log = new Log({
-        log: JSON.stringify(req.body.moneyRequestUpdates[0])
+        log: JSON.stringify(updates)
     });
     Log.createLog(log, function(err, newLog){
         console.log(newLog);
     });
-  var updates = req.body.moneyRequestUpdates[0];
   var status = updates.state;
   var requestId = updates.sourceMoneyRequestId;
 
