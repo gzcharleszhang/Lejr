373ab9df8c1618aebfb6a4e8b3c1c70ae7872ea9
Charles Zhang <gzcharleszhang@hotmail.com>
2018-01-21 00:21:10 -0500

    Cool updates

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var User = require('../models/user');
 var Ledger = require('../models/ledger');
+var request = require('request');
 
 /* POST method for user registration */
 router.post('/register', function(req, res, next) {
@@ -24,6 +25,7 @@ router.post('/register', function(req, res, next) {
       success: true,
         message: "Successfully registered user"
     };
+
     res.send(response);
   });
 });
@@ -59,10 +61,95 @@ router.post('/login', function(req, res){
 
 // POST method for money request
 router.post('/request', function(req, res){
+    function makeid() {
+        var text = "";
+        var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+        for (var i = 0; i < 10; i++)
+            text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+        return text;
+    }
   var sender = req.body.sender;
   var recipient = req.body.recipient;
   var amount = req.body.amount;
-  var sourceMoneyRequestId = req.body.sourceMoneyRequestId;
+  var sourceMoneyRequestId = makeid();
+
+    // Set the headers
+    var headers = {
+        'thirdPartyAccessId': 'CA1TAUCyPsSfqkzX',
+        'salt': 'jason',
+        'secretKey': 'bkSFj52Ip9BLPbW+swg5oUAuA2pI2B0Lr4PENEmR1s0='
+    };
+
+    // Configure the request
+    var options = {
+        url: 'https://gateway-web.beta.interac.ca/publicapi/api/v1/access-tokens',
+        method: 'GET',
+        headers: headers
+    };
+    request(options, function (error, response, body) {
+        if (!error && response.statusCode === 200) {
+            // Print out the response body
+            console.log(body);
+        }
+        var token = JSON.parse(body);
+        headers = {
+          accessToken: 'Bearer ' + token.access_token,
+            thirdPartyAccessId: 'CA1TAUCyPsSfqkzX',
+            requestId: sourceMoneyRequestId,
+            deviceId: 'jason',
+            apiRegistrationId: 'CA1ARhhdGyUQ2kqj',
+            'content-type': 'application/json'
+        };
+        var reqBody = {
+            "referenceNumber": "string",
+            "sourceMoneyRequestId": sourceMoneyRequestId,
+            "requestedFrom": {
+                "contactName": "Jason",
+                "language": "en",
+                "notificationPreferences": [
+                    {
+                        "handle": "jason368@hotmail.ca",
+                        "handleType": "email",
+                        "active": true
+                    }
+                ]
+            },
+            "amount": 100,
+            "currency": "CAD",
+            "editableFulfillAmount": false,
+            "requesterMessage": "Pay up!",
+            "invoice": {
+                "invoiceNumber": "string",
+                "dueDate": "2018-02-01T16:12:12.000Z"
+            },
+            "expiryDate": "2018-02-01T16:12:12.000Z",
+            "supressResponderNotifications": true,
+            "returnURL": "string",
+            "creationDate": "string",
+            "status": 0,
+            "fulfillAmount": 0,
+            "responderMessage": "string",
+            "notificationStatus": 0
+        };
+        options = {
+            url: 'https://gateway-web.beta.interac.ca/publicapi/api/v2/money-requests/send',
+            method: 'POST',
+            body: JSON.stringify(reqBody),
+            headers: headers
+        };
+
+        request(options, function (err, res, body){
+            if (!err && res.statusCode === 200) {
+                // Print out the response body
+                console.log(body);
+            } else {
+              console.log(body);
+            }
+        })
+
+    });
 
   var ledger = new Ledger({
       sender: sender,
@@ -78,6 +165,7 @@ router.post('/request', function(req, res){
       success: true,
         message: "Successfully created ledger"
     };
+
     res.send(response);
   })
 });
@@ -93,7 +181,7 @@ router.get('/ledger', function(req, res){
 
 // POST method for notifications (callback from Interac)
 router.post('/notifications', function(req, res, next){
-  var updates = req.body.body.moneyRequestUpdates;
+  var updates = req.body.moneyRequestUpdates;
   var status = updates.state;
   var requestId = updates.sourceMoneyRequestId;
   if (status === "COMPLETED"){
@@ -105,6 +193,7 @@ router.post('/notifications', function(req, res, next){
       res.send("success")
     })
   } else {
+
       res.send("error")
   }
 });
